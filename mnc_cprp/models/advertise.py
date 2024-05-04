f232e30b5f78030d9c7621baa24c316cf20b9a6a
najib arif <najib.arif16@gmail.com>
2024-05-04 15:35:22 +0700

    add sql cons adv table

--- mnc_cprp/models/advertise.py
+++ mnc_cprp/models/advertise.py
@@ -3,6 +3,9 @@ from odoo import api, models, fields, _
 class CPRPAdvertise(models.Model):
     _name = "cprp.advertise"
     _description = "Advertise CPRP"
+    _sql_constraints = [
+        ("name_unique", "unique(name)", "Name must be unique")
+    ]
 
     name = fields.Char(required=True)
     code = fields.Char()
